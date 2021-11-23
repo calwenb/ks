b09af3fcf8798b1077264340052e380592cb3a7f
Mr.文 <2280400645@qq.com>
2021-11-23 15:19:55 +0800

    wen1

--- src/main/java/wen/test/MyTest.java
+++ src/main/java/wen/test/MyTest.java
@@ -2,7 +2,6 @@ package wen.test;
 
 import org.junit.Test;
 import wen.dao.UserDao;
-import wen.pojo.Order;
 import wen.pojo.User;
 import wen.service.OrderService;
 import wen.utils.DruidUtil;
@@ -11,7 +10,6 @@ import wen.utils.MailUtil;
 
 import javax.mail.MessagingException;
 import java.io.UnsupportedEncodingException;
-import java.lang.reflect.InvocationTargetException;
 import java.security.GeneralSecurityException;
 import java.sql.Connection;
 import java.sql.SQLException;
@@ -33,16 +31,18 @@ public class MyTest {
         users.forEach(System.out::println);
         //UserDao.addUser(con, new User(45, "whl", "long", "123", 2, 10010, "2241241@qq.com"));
     }
+
     @Test
     public void t2() throws Exception {
         System.out.println("=======================");
         System.out.println(OrderService.orderCount());
     }
+
     @Test
     public void t3() throws MessagingException, GeneralSecurityException, UnsupportedEncodingException, InterruptedException {
         for (int i = 0; i < 5; i++) {
             Thread.sleep(1000);
-            MailUtil.sendMail("mr.wen66@qq.com",i+"s",i+"s",i+"s");
+            MailUtil.sendMail("mr.wen66@qq.com", i + "s", i + "s", i + "s");
         }
     }
 }
